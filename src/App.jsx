278e7b25dec4e73fbd54cfe2bf0a9e3afa3b8255
BaGorK <edmealemkassahun@gmail.com>
2024-03-04 22:50:24 +0300

    feat: implementing dark theme - initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import Gallery from './Gallery';
 import SearchForm from './SearchForm';
 import ThemeToggle from './ThemeToggle';
-import { useGlobalContext } from './context';
 
 const App = () => {
-  const { testProvider } = useGlobalContext();
-  console.log(testProvider);
   return (
     <main>
       <Gallery />
