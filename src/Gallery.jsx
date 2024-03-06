b71f3234aa2c1da603830b0e4b6a659c4d68e1a9
BaGorK <edmealemkassahun@gmail.com>
2024-03-06 23:07:22 +0300

    feat: setting up react-query and images query

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,4 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const url =
+  'https://api.unsplash.com/photos/?client_id=FOqwuN2_SzmtLji7mlVPyAmXODkYIBseHkV6ahhuGIc&query=cat';
+
 function Gallery() {
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(url);
+
+      return result.data;
+    },
+  });
+
+  console.log(response);
+
   return <h2>gallery</h2>;
 }
 
