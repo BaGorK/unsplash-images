724fa71accbe5e7a86af744eed8f9f3d75ad7304
BaGorK <edmealemkassahun@gmail.com>
2024-03-06 23:35:56 +0300

    feat: handling different status loading, error

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
 const url =
-  'https://api.unsplash.com/photos/?client_id=FOqwuN2_SzmtLji7mlVPyAmXODkYIBseHkV6ahhuGIc&query=cat';
+  'https://api.unsplash.com/photos/?client_id=FOqwuN2_SzmtLji7mlVPyAmXODkYIBseHkV6ahhuGIc&query=cfa;jfsd;aat';
 
 function Gallery() {
   const response = useQuery({
@@ -14,7 +14,31 @@ function Gallery() {
     },
   });
 
-  console.log(response);
+  if (response.isLoading) {
+    return (
+      <section className='image-container'>
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+
+  if (response.isError) {
+    return (
+      <section className='image-container'>
+        <h4>There was an error...</h4>
+      </section>
+    );
+  }
+
+  if (response.data.length < 1) {
+    return (
+      <section className='image-container'>
+        <h4>No result found...</h4>
+      </section>
+    );
+  }
+
+  console.log(response.data);
 
   return <h2>gallery</h2>;
 }
